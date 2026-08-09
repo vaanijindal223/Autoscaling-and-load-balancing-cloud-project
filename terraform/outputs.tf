600ccd7e50619d09686e22b2b5b56c773744a5ed
vaanijindal223 <vaanijindal223@gmail.com>
2026-08-10 00:29:36 +0530

    till phase 5

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -37,3 +37,19 @@ output "ecs_task_role_arn" {
 output "app_bucket_name" {
   value = aws_s3_bucket.app_bucket.bucket
 }
+
+output "backend_ecr_repository_url" {
+  value = aws_ecr_repository.backend.repository_url
+}
+
+output "frontend_ecr_repository_url" {
+  value = aws_ecr_repository.frontend.repository_url
+}
+
+output "backend_asg_name" {
+  value = aws_autoscaling_group.backend.name
+}
+
+output "backend_launch_template_id" {
+  value = aws_launch_template.backend.id
+}
